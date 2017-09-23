bdde13648907f334f0286880c030c21511b5b7fb
James Starkman <-1@case.edu>
2017-09-23 06:06:46 +0000

    Publishes mV values under /chip_adc_mac/millivolts/0

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-/* #include <ros/ros.h> */
-
 #include <fcntl.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -10,6 +8,10 @@
 #include <sys/ioctl.h>
 
 #include <linux/i2c-dev.h>
+
+
+#include <ros/ros.h>
+#include <std_msgs/Int32.h>
 
 
 #define OFFSET 0x00 /* use 0x01 for 0.7V--2.7V) */
@@ -65,13 +67,59 @@ uint32_t read_adc() {
 	return millivolts;
 }
 
+
+char* get_mac(const char* iface) {
+	const int len_mac = 18;
+	int fd;
+	char fn[100];
+	char* mac = (char*) malloc(len_mac);
+	fn[0] = 0;
+	strcat(fn, "/sys/class/net/");
+	strcat(fn, iface);
+	strcat(fn, "/address");
+	fd = open(fn, O_RDONLY);
+	if (fd < 0) {
+		perror(fn);
+		exit(16);
+	}
+	if (read(fd, mac, len_mac) != len_mac) {
+		perror(mac);
+		exit(17);
+	}
+	return mac;
+}
+
+
 int main(int argc, char** argv) {
 	uint32_t millivolts = 0;
+	char* mac = get_mac("wlan0");
+	char buf[128];
+	int i = 17; /* length of MAC */
+
+	mac[i] = 0; /* remove newline */
+	while (i-->0) {
+		if (mac[i] == ':') {
+			mac[i] = '_';
+		}
+	}
+	buf[0] = 0;
+	strcat(buf, "chip_adc_");
+	strcat(buf, mac);
+	ros::init(argc, argv, buf);
+	ros::NodeHandle n("~");
+	ros::Publisher pub = n.advertise<std_msgs::Int32>("millivolts/0", 1);
+	std_msgs::Int32 adc_output;
+	adc_output.data = 0;
+
 	open_adc(0x34);
 	enable_adc();
 	while (1) {
 		millivolts = read_adc();
 		printf("ADC value: %d millivolts\n", millivolts);
+
+		adc_output.data = millivolts;
+		pub.publish(adc_output);
+
 		nanosleep(&sample_period, NULL);
 	}
 	return 0;
